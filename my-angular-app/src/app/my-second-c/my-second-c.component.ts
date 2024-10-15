7e0800fa3a7e812b2e0d95e0558ec5bfc7b8f136
aatrusov <trusovandrey.aleks@gmail.com>
2024-10-15 20:28:02 +0200

    ngIf / ngFor / [ngClass] / (clock) / [ngStyle] / [disabled]
    class - view - element data binding

--- my-angular-app/src/app/my-second-c/my-second-c.component.ts
+++ my-angular-app/src/app/my-second-c/my-second-c.component.ts
@@ -1,15 +1,37 @@
 import { Component } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {NgClass, NgForOf, NgIf, NgStyle} from '@angular/common';
 
 @Component({
   selector: 'app-my-second-c',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    NgForOf,
+    NgIf,
+    NgClass,
+    NgStyle
   ],
   templateUrl: './my-second-c.component.html',
   styleUrl: './my-second-c.component.css'
 })
 export class MySecondCComponent {
   value: string = "";
+
+  isBul: boolean = false;
+
+  items: string[] = ['Item 1', 'Item 2', 'Item 3'];
+
+  isActive: boolean = true;
+
+  isHighlighted: boolean = false;
+
+  handleClick() {
+    this.value = 'Button was clicked!';
+  }
+
+  clickToAppearList() {
+    this.isBul = !this.isBul;
+  }
+
 }
