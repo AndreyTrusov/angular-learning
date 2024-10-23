af303f3e7194ff778e9ebd5a72fe1a96b8d7b047
aatrusov <trusovandrey.aleks@gmail.com>
2024-10-23 13:13:02 +0200

    children path into different file / queryParams / filtering by param

--- routing-project/src/app/app.routes.ts
+++ routing-project/src/app/app.routes.ts
@@ -1,17 +1,13 @@
 import {Routes} from "@angular/router";
+import {routes as userRoutes} from "./users/users.routes";
 import {TasksComponent} from "./tasks/tasks.component";
 import {NoTaskComponent} from "./tasks/no-task/no-task.component";
 import {UserTasksComponent} from "./users/user-tasks/user-tasks.component";
-import {NewTaskComponent} from "./tasks/new-task/new-task.component";
 import {NotFoundComponent} from "./not-found/not-found.component";
 
 export const ROUTES: Routes = [
   {path: 'tasks', component: TasksComponent},
-  {path: 'users/:userId', component: UserTasksComponent, children: [
-      {path: '', redirectTo: 'tasks', pathMatch: 'prefix'},
-      {path: 'tasks', component: TasksComponent},
-      {path: 'tasks/new', component: NewTaskComponent},
-    ]},
+  {path: 'users/:userId', component: UserTasksComponent, children: userRoutes},
   {path: '', component: NoTaskComponent},
   {path: '**', component: NotFoundComponent},
 ]
--- routing-project/src/app/tasks/tasks.component.ts
+++ routing-project/src/app/tasks/tasks.component.ts
@@ -1,21 +1,44 @@
-import {Component, computed, inject, input} from '@angular/core';
+import {Component, computed, DestroyRef, inject, input, signal} from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import {TasksService} from "./tasks.service";
+import {ActivatedRoute, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent {
   userId = input.required<string>();
-
+  // order = input<'asc' | 'desc'>();
+  // order?: 'asc' | 'desc';
+  order = signal<'asc' | 'desc'>('desc');
   // reading tasks from service
   private tasksService = inject(TasksService);
   userTasks = computed( () =>
-    this.tasksService.allTasks().filter((task) => task.userId === this.userId()),
+    this.tasksService
+      .allTasks()
+      .filter((task) => task.userId === this.userId())
+      .sort((a, b) => {
+        if(this.order() === 'desc'){
+          return a.id > b.id ? -1 : 1;
+        } else{
+          return a.id > b.id ? 1 : -1;
+        }
+      }),
   )
+
+  private activatedRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void{
+    const subscription = this.activatedRoute.queryParams.subscribe({
+      next: params => this.order.set(params['order']), // this will be updated whenever query param is changed
+    })
+
+    this.destroyRef.onDestroy( () => subscription.unsubscribe());
+  }
 }
