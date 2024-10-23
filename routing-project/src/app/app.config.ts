3e7e24e22d7453a9e9f2a39391fc884a8f2045e2
aatrusov <trusovandrey.aleks@gmail.com>
2024-10-23 12:43:14 +0200

    routes children / linking around.. / loading tasks

--- routing-project/src/app/app.config.ts
+++ routing-project/src/app/app.config.ts
@@ -1,9 +1,11 @@
 import {ApplicationConfig} from "@angular/core";
-import {provideRouter, withComponentInputBinding} from "@angular/router";
+import {provideRouter, withComponentInputBinding, withRouterConfig} from "@angular/router";
 import {ROUTES} from "./app.routes";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(ROUTES, withComponentInputBinding()),
+    provideRouter(ROUTES, withComponentInputBinding(), withRouterConfig({
+      paramsInheritanceStrategy: 'always' // params are shown also in child routes
+    })),
   ]
 }
--- routing-project/src/app/app.routes.ts
+++ routing-project/src/app/app.routes.ts
@@ -2,9 +2,16 @@ import {Routes} from "@angular/router";
 import {TasksComponent} from "./tasks/tasks.component";
 import {NoTaskComponent} from "./tasks/no-task/no-task.component";
 import {UserTasksComponent} from "./users/user-tasks/user-tasks.component";
+import {NewTaskComponent} from "./tasks/new-task/new-task.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
 export const ROUTES: Routes = [
   {path: 'tasks', component: TasksComponent},
-  {path: 'users/:userId', component: UserTasksComponent},
+  {path: 'users/:userId', component: UserTasksComponent, children: [
+      {path: '', redirectTo: 'tasks', pathMatch: 'prefix'},
+      {path: 'tasks', component: TasksComponent},
+      {path: 'tasks/new', component: NewTaskComponent},
+    ]},
   {path: '', component: NoTaskComponent},
+  {path: '**', component: NotFoundComponent},
 ]
