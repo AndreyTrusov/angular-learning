16b23a7fd3eecee4861330e7af7d3d68f8e96aaa
aatrusov <trusovandrey.aleks@gmail.com>
2024-11-02 09:00:28 +0100

    reactive way to handle forms - starting

--- forms-project/src/app/app.component.ts
+++ forms-project/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 
-import { LoginComponent } from './auth/login/login.component';
+import {LoginComponentReactive} from "./auth/login-reactive/login.component.reactive";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
-  imports: [LoginComponent],
+  imports: [LoginComponentReactive],
 })
 export class AppComponent {}
