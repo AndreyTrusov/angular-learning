bb7e10082dc9c30ba9bc4b6c523d46d1487fd6f2
aatrusov <trusovandrey.aleks@gmail.com>
2024-11-05 10:27:43 +0100

    connecting angular to backend / GET to fetch data / configuring HTTP request / Loading fallback / Handling errors /

--- http_requests/src/app/places/available-places/available-places.component.ts
+++ http_requests/src/app/places/available-places/available-places.component.ts
@@ -1,8 +1,10 @@
-import { Component, signal } from '@angular/core';
+import {Component, DestroyRef, inject, OnInit, signal} from '@angular/core';
 
-import { Place } from '../place.model';
-import { PlacesComponent } from '../places.component';
-import { PlacesContainerComponent } from '../places-container/places-container.component';
+import {Place} from '../place.model';
+import {PlacesComponent} from '../places.component';
+import {PlacesContainerComponent} from '../places-container/places-container.component';
+import {HttpClient} from "@angular/common/http";
+import {catchError, map, throwError} from "rxjs";
 
 @Component({
   selector: 'app-available-places',
@@ -11,6 +13,43 @@ import { PlacesContainerComponent } from '../places-container/places-container.c
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit {
+  isFetching = signal(false);
+  error = signal('');
   places = signal<Place[] | undefined>(undefined);
+  private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
+
+  // contructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.isFetching.set(true);
+    const subscription = this.httpClient.get<{ places: Place[] }>('http://localhost:3000/places', {
+      observe: 'response'
+    }).pipe(
+      map((resData) => resData.body?.places),
+      catchError((error, obs) => {
+        console.log(error, obs);
+        return throwError(
+          () => new Error("Something went wrong!"),
+        )
+      })
+    ).subscribe({
+      next: (resData) => {
+        console.log(resData);
+        this.places.set(resData);
+      },
+      error: (error: Error) => {
+        console.log(error); // sending error message to analytical server maybe
+        this.error.set(error.message);
+      },
+      complete: () => {
+        this.isFetching.set(false);
+      }
+    });
+
+    this.destroyRef.onDestroy(() => {
+      subscription.unsubscribe();
+    })
+  }
 }
